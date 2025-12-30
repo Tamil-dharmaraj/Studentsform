3cd2201c73360fbb3f262876414b8d2905287b3f
Tamil Dharmaraj <tamildharmaraj@gmail.com>
2025-12-30 14:31:16 +0530

    change

--- src/Form/StudentsRegistration.tsx
+++ src/Form/StudentsRegistration.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const StudentsRegistration: React.FC = () => {
 const [successMessage, setSuccessMessage] = useState("");
+console.log("successMessage",  successMessage)
 
 const handleSubmit = (e: React.FormEvent) => {
   e.preventDefault();
